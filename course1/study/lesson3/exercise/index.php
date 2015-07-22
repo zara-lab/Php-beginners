de65ac41691436b53b7cffb7c2532f3b3814a2a8
Miroslav Yovchev <m.yovchev@corllete.com>
2015-07-22 17:56:09 +0300

    exercise 3

---
+++ course1/study/lesson3/exercise/index.php
@@ -0,0 +1,18 @@
+<html>
+<?php
+    $title = ""; // Site title
+    $pageTitle = ""; // This page title
+    $customers = array(); // All customers array
+?>
+<head>
+    <title><!-- Site title --></title>
+</head>
+<body>
+    <h1>
+        <!-- Page title 'Customer Index' -->
+    </h1>
+    <ul>
+      <li><!-- For every customer new li tag - name, phone and email, "view more" link --></li>
+    </ul>
+</body>
+</html>
---
+++ course1/study/lesson3/exercise/view.php
@@ -0,0 +1,26 @@
+<html>
+<?php
+    $title = ""; // Site title
+    $pageTitle = ""; // This page title
+    $customers = array(); // All customers array
+    $customerId = null; // Current customer ID
+    $customer = array(); // Current customer array
+?>
+<head>
+    <title><!-- Site title --></title>
+</head>
+<body>
+    <h1>
+        <!-- Page title 'Customer Profile' -->
+    </h1>
+    <div>
+        <!-- customer names, phone, email, age, picture, balance -->
+    </div>
+    <p>
+        <!-- customer 'about' -->
+    </p>
+    <p>
+        <a href="#back-link" title="Back to the index">Back</a>
+    </p>
+</body>
+</html>
