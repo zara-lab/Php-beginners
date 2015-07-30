dbc59c2ff3e22dfc96f6a6362b9f7c39fc67b6b5
Atanaska <a.mateva@yahoo.com>
2015-07-30 20:59:22 +0300

    [lesson2][exercise] solution by matevaai

---
+++ course1/users/matevaai/exercies 2/tasks/task2.php
@@ -0,0 +1,4 @@
+<?php
+
+$search_index = array_search('Search me', $array);
+printc("Task Array Search: index is $search_index");
---
+++ course1/users/matevaai/lesson2/exercies/data/array.php
@@ -0,0 +1,8 @@
+<?php
+
+$array = array(
+	'First elememt',
+	'Search me',
+	'I love ZaraLab',
+	'I\'m going to become PHP expert!'
+	);
